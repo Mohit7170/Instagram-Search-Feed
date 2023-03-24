5420f966a6614613da9d9e71b64f4bba9697763b
MoDEV <mohit71703@gmail.com>
2023-03-24 18:00:45 +0530

    pagination done

--- app/src/main/java/com/app/instagramfeed/ui/activities/MainActivity.kt
+++ app/src/main/java/com/app/instagramfeed/ui/activities/MainActivity.kt
@@ -2,10 +2,12 @@ package com.app.instagramfeed.ui.activities
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.app.instagramfeed.custom.SpannedGridLayoutManager
 import com.app.instagramfeed.databinding.ActivityMainBinding
 import com.app.instagramfeed.ui.adapter.GridAdapter
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,10 +21,14 @@ class MainActivity : AppCompatActivity() {
 
         activity = this@MainActivity
 
+        var i = 10
+        var j = 1
+
         val manager = SpannedGridLayoutManager(
             object : SpannedGridLayoutManager.GridSpanLookup {
                 override fun getSpanInfo(position: Int): SpannedGridLayoutManager.SpanInfo {
                     // Conditions for 2x2 items
+                    Log.d(TAG, "getSpanInfo: position -- $position")
                     return if (position % 6 == 0 || position % 6 == 4) {
                         SpannedGridLayoutManager.SpanInfo(2, 2)
                     } else {
@@ -31,16 +37,35 @@ class MainActivity : AppCompatActivity() {
                 }
             },
             3,  // number of columns
-            1.2f // how big is default item
+            0.95f // how big is default item
         )
 
-
-        val adapter = GridAdapter()
+        val adapter = GridAdapter(binding.searchRv)
 
         binding.searchRv.apply {
             layoutManager = manager
             this.adapter = adapter
+
+//            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+//                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+//                    super.onScrolled(recyclerView, dx, dy)
+//
+//                    Log.d(TAG, "onScrolled: recyclerView.canScrollVertically(1) -- ${recyclerView.canScrollVertically(1)}   dy -- ${dy}")
+////                    if (!recyclerView.canScrollVertically(-1) && dy < 0) {
+//                    if (!recyclerView.canScrollVertically(RecyclerView.FOCUS_DOWN)) {
+//
+//                        adapter.addData(10)
+//                        Log.d(TAG, "onScrolled: BOTTOM")
+////                        recyclerView.invalidate()
+//                    }
+//                }
+//            })
         }
 
     }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
 }
--- app/src/main/java/com/app/instagramfeed/ui/adapter/SearchAdapter.kt
+++ app/src/main/java/com/app/instagramfeed/ui/adapter/SearchAdapter.kt
@@ -5,7 +5,9 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.app.instagramfeed.databinding.SearchIcBinding
 
-class GridAdapter : RecyclerView.Adapter<GridAdapter.ViewHolder>() {
+class GridAdapter(val recyclerView: RecyclerView) : RecyclerView.Adapter<GridAdapter.ViewHolder>() {
+
+    var items = 30
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = SearchIcBinding.inflate(
@@ -18,13 +20,28 @@ class GridAdapter : RecyclerView.Adapter<GridAdapter.ViewHolder>() {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
+        if (position == items - 1) {
+            addData(10)
+            return
+        }
+
         holder.binding.apply {
-            tv.text = position.toString()
+            tv.text = (position + 1).toString()
         }
     }
 
-    override fun getItemCount(): Int = 15
+    override fun getItemCount(): Int = items
 
+    private fun addData(item: Int) {
+
+        val oldItem = items
+        items += item
+
+        recyclerView.post {
+            notifyItemRangeInserted(oldItem, item)
+        }
+
+    }
 
     inner class ViewHolder(val binding: SearchIcBinding) :
         RecyclerView.ViewHolder(binding.root)
